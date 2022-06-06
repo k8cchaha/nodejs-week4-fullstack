1b077e31985199a7c6181cfbc1da6c777ec821d6
Alex_Wang <k8cchaha@gmail.com>
2022-06-06 19:02:06 +0800

    enable prod connect

--- connections/index.js
+++ connections/index.js
@@ -3,21 +3,21 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-// const DB = process.env.DATABASE.replace(
-//   "<password>",
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
-// mongoose
-//   .connect(DB)
-//   .then(() => {
-//     console.log("連線成功");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("連線成功");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //For local testing
-mongoose.connect("mongodb://localhost:27017/hotel").then(() => {
-  console.log("連線成功");
-});
+// mongoose.connect("mongodb://localhost:27017/hotel").then(() => {
+//   console.log("連線成功");
+// });
